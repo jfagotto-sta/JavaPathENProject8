ab4317e56879e40d39bcc22dc8df737049cdbbda
Fagotto <joffrey.fagotto4@laposte.net>
2022-11-03 17:51:07 +0100

    method to get 5 nearest attractions

--- TourGuide/src/test/java/tourGuide/TestPerformance.java
+++ TourGuide/src/test/java/tourGuide/TestPerformance.java
@@ -2,10 +2,7 @@ package tourGuide;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.time.StopWatch;
@@ -25,52 +22,57 @@ import tourGuide.user.UserReward;
 
 
 public class TestPerformance {
-	
+
 	/*
 	 * A note on performance improvements:
-	 *     
+	 *
 	 *     The number of users generated for the high volume tests can be easily adjusted via this method:
-	 *     
+	 *
 	 *     		InternalTestHelper.setInternalUserNumber(100000);
-	 *     
-	 *     
+	 *
+	 *
 	 *     These tests can be modified to suit new solutions, just as long as the performance metrics
-	 *     at the end of the tests remains consistent. 
-	 * 
+	 *     at the end of the tests remains consistent.
+	 *
 	 *     These are performance metrics that we are trying to hit:
-	 *     
+	 *
 	 *     highVolumeTrackLocation: 100,000 users within 15 minutes:
 	 *     		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
      *
      *     highVolumeGetRewards: 100,000 users within 20 minutes:
 	 *          assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	 */
-	
-	@Ignore
+
 	@Test
 	public void highVolumeTrackLocation() {
+		Locale.setDefault(Locale.US);
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15 minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(100000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		List<User> allUsers = new ArrayList<>();
 		allUsers = tourGuideService.getAllUsers();
-		
+
 	    StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
+
+
 		for(User user : allUsers) {
-			tourGuideService.trackUserLocation(user);
+			MonThread thread = new MonThread();
+			thread.init(user, tourGuideService);
+			thread.start();
+
+			//tourGuideService.trackUserLocation(user);
 		}
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
-		System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds."); 
+		System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
 		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
-	
-	@Ignore
+
 	@Test
 	public void highVolumeGetRewards() {
 		GpsUtil gpsUtil = new GpsUtil();
@@ -81,22 +83,36 @@ public class TestPerformance {
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-		
+
 	    Attraction attraction = gpsUtil.getAttractions().get(0);
 		List<User> allUsers = new ArrayList<>();
 		allUsers = tourGuideService.getAllUsers();
 		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
-	     
+
 	    allUsers.forEach(u -> rewardsService.calculateRewards(u));
-	    
+
 		for(User user : allUsers) {
 			assertTrue(user.getUserRewards().size() > 0);
 		}
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
-		System.out.println("highVolumeGetRewards: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds."); 
+		System.out.println("highVolumeGetRewards: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
 		assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
-	
+
+	public class MonThread extends Thread {
+		User user;
+		TourGuideService tourGuideService;
+		public void init(User user, TourGuideService tourGuideService) {
+			this.user = user;
+			this.tourGuideService = tourGuideService;
+		}
+		public void run() {
+			tourGuideService.trackUserLocation(user);
+		}
+	}
 }
+
+
+
